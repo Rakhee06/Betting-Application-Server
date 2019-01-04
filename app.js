1ab0f44727c6969f5459b8869fe2b7a07a8edcd7
Rakhee Singh <rakhee@Rakhees-MacBook-Pro.local>
2019-01-04 13:24:07 -0500

    Adding files to the project.

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const session = require("express-session");
+const cors = require("cors");
+const routes = require("./routes");
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(bodyparser.json());
+mongoose.connect("mongodb://skanda25011993:cs697project@ds157223.mlab.com:57223/cs697project");
+
+app.use(
+    cors({
+        origin : "http://localhost:3000",
+        methods:["GET","HEAD","POST","DELETE","PUT","PATCH","OPTIONS"],
+        credentials:true
+    })
+);
+
+app.use(
+    session({
+        secret:"supersecretstring12345!",
+        saveUninitialized: true,
+        resave: true,
+        cookie:{maxAge: 60000 * 30}
+    })
+);
+routes(app);
+app.get("/",(req,res)=>  res.json({hi:"there"})
+);
+// app.get("/", (req, res) => res.json("sdasdsa"));
+app.listen(port);
+console.log(process.env.IP);
